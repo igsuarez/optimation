b57252cc60f3eec16b6c894df8eed5ba4bd0845c
Ismael <ismael@eight-wire.com>
2020-12-21 15:26:28 +1300

    Add Taxes Service interface and implementation for GST calculations

---
+++ ProcessingService.Api/Infrastructure/Services/ITaxesService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessingService.Api.Infrastructure.Services
+{
+    public interface ITaxesService
+    {
+        /// <summary>
+        /// Subtract GST from value which includes GST
+        /// </summary>
+        /// <param name="value">Value includes GST</param>
+        /// <returns>Value without GST</returns>
+        double SubtractGST(double @value);
+    }
+}
---
+++ ProcessingService.Api/Infrastructure/Services/TaxesService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessingService.Api.Infrastructure.Services
+{
+    public class TaxesService : ITaxesService
+    {
+        const double GST_DIVISOR = 1.15;
+
+        public double SubtractGST(double @value)
+        {
+            return value / GST_DIVISOR;
+        }
+    }
+}
