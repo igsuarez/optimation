b63f8a75069acea1a2c6490c4fc5a3900ca2b4bd
Ismael <ismael@eight-wire.com>
2020-12-21 17:14:11 +1300

    Add xml service to extract and validate xml data coming from raw text

---
+++ ProcessingService.Api/Infrastructure/Services/IXmlService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessingService.Api.Infrastructure.Services
+{
+    public interface IXmlService
+    {
+        /// <summary>
+        /// Extract all xml tags present in the text
+        /// </summary>
+        /// <param name="text">Raw text with xml embedded</param>
+        /// <returns>List of xml tag found</returns>
+        List<string> ExtractXmlTags(string text);
+
+        /// <summary>
+        /// Extract all xml nodes present in the text
+        /// </summary>
+        /// <param name="text">Raw text with xml embedded</param>
+        /// <returns>xml nodes as text</returns>
+        string ExtractXmlNodes(string text);
+
+        /// <summary>
+        /// Check existence of a xml tag in the text
+        /// </summary>
+        /// <param name="text">Raw text with xml embedded</param>
+        /// <param name="xmlTag">Xml tag to be checked</param>
+        /// <returns>true, tag exists otherwise false</returns>
+        bool XmlTagExist(string text, string xmlTag);
+
+        /// <summary>
+        /// Check if text is a xml well formed
+        /// </summary>
+        /// <param name="text">Supposed xml text</param>
+        /// <returns>true, text is a xml well formed otherwise false</returns>
+        bool IsXmlWellFormed(string text);
+
+        string GetXmlNodeValue(string xmlText, string xmlTag);
+    }
+}
---
+++ ProcessingService.Api/Infrastructure/Services/XmlService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ProcessingService.Api.Infrastructure.Services
+{
+    public class XmlService : IXmlService
+    {
+        private const string TAG_PATTERN = @"<[^>]+>|\+";
+        private const string NODES_PATTERN = @"(<[^>/]+>)(.+?)(</[^>]+>)";
+
+        public List<string> ExtractXmlTags(string text)
+        {
+            Regex regex = new Regex(TAG_PATTERN);
+            MatchCollection matches = regex.Matches(text);
+            return matches.Select(x => x.Value).ToList();
+        }
+
+        public string ExtractXmlNodes(string text)
+        {
+            text = text.Replace(Environment.NewLine, string.Empty);
+            Regex regex = new Regex(NODES_PATTERN);
+            MatchCollection matches = regex.Matches(text);
+            return string.Join(String.Empty, matches.Select(x => x.Value).ToArray());
+        }
+
+        public bool XmlTagExist(string text, string xmlTag)
+        {
+            text = text.Replace(Environment.NewLine, String.Empty);
+            Regex regex = new Regex($"(<{xmlTag}>)(.+?)(</{xmlTag}>)");
+            Match match = regex.Match(text);
+
+            return match.Success;
+        }
+
+        public bool IsXmlWellFormed(string text)
+        {
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.LoadXml(text);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string GetXmlNodeValue(string xmlText, string xmlTag)
+        {
+            try
+            {
+                var xmlDocument = new XmlDocument();
+                xmlDocument.LoadXml(xmlText);
+
+                var xmlNode = xmlDocument.SelectSingleNode($"//{xmlTag}");
+                return xmlNode != null ? xmlNode.InnerText : String.Empty;
+            }
+            catch
+            {
+                return String.Empty;
+            }
+        }
+    }
+}
