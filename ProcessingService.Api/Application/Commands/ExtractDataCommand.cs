b71d5047174a361067f600a77b8c74e85e66c4b9
Ismael <ismael@eight-wire.com>
2020-12-21 17:42:03 +1300

    Add logic on command and command handler

--- ProcessingService.Api/Application/Commands/ExtractDataCommand.cs
+++ ProcessingService.Api/Application/Commands/ExtractDataCommand.cs
@@ -2,12 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace ProcessingService.Api.Application.Commands
 {
     public class ExtractDataCommand : IRequest<ExtractDataResult>
     {
-        public string RawData { get; private set; }
+        [DataMember]
+        public string RawData { get; set; }
     }
 }
--- ProcessingService.Api/Application/Commands/ExtractDataCommandHandler.cs
+++ ProcessingService.Api/Application/Commands/ExtractDataCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using ProcessingService.Api.Infrastructure.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,29 +12,61 @@ namespace ProcessingService.Api.Application.Commands
         : IRequestHandler<ExtractDataCommand, ExtractDataResult>
     {
         private readonly IMediator _mediator;
+        private readonly IXmlService _xmlService;
+        private readonly ITaxesService _taxesService;
+        private static string[] _mandatoryTags = new string[] { "total", "cost_centre" };
+        private const string TOTAL_TAG = "total";
 
-        public ExtractDataCommandHandler(IMediator mediator)
+        public ExtractDataCommandHandler(IMediator mediator, IXmlService xmlService, ITaxesService taxesService)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _xmlService = xmlService ?? throw new ArgumentNullException(nameof(xmlService));
+            _taxesService = taxesService ?? throw new ArgumentNullException(nameof(taxesService));
         }
 
         public Task<ExtractDataResult> Handle(ExtractDataCommand message, CancellationToken cancellationToken)
         {
+            try
+            {
+                foreach (var tag in _mandatoryTags)
+                {
+                    if (!_xmlService.XmlTagExist(message.RawData, tag))
+                    {
+                        throw new InvalidInputException($"Invalid input, missing tag: {tag}", null);
+                    }
+                }
 
+                string xmlData = _xmlService.ExtractXmlNodes(message.RawData);
+                if (!_xmlService.IsXmlWellFormed($"<root>{xmlData}</root>"))
+                {
+                    throw new InvalidInputException($"Invalid input, xml wrong format: {xmlData}", null);
+                }
 
+                string totaltext = _xmlService.GetXmlNodeValue($"<root>{xmlData}</root>", TOTAL_TAG);
+                double total;
+                if (!double.TryParse(totaltext, out total))
+                {
+                    throw new InvalidInputException($"Invalid input, invalid value on total node: {xmlData}", null);
+                }
 
-            return Task.FromResult(ExtractDataResult.Create("", 1, 1));
+                double totalWithoutGST = _taxesService.SubtractGST(total);
+                return Task.FromResult(ExtractDataResult.Create(xmlData, totalWithoutGST, total - totalWithoutGST));
+
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidInputException($"Error during processing data. Input: {message.RawData}", ex); ;
+            }
         }
     }
-
 
     public class ExtractDataResult
     {
         public string ExtractedData { get; set; }
-        public decimal Total { get; set; }
-        public decimal GST { get; set; }
+        public double Total { get; set; }
+        public double GST { get; set; }
 
-        public static ExtractDataResult Create(string extractedData, decimal total, decimal gst)
+        public static ExtractDataResult Create(string extractedData, double total, double gst)
         {
             return new ExtractDataResult()
             {
@@ -43,6 +76,13 @@ namespace ProcessingService.Api.Application.Commands
             };
         }
 
+    }
+
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException(string message, Exception exception)
+            : base(message, exception)
+        { }
     }
 
 
